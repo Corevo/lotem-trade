1f3a5c0286da5fbfffe4c27549bcfb84e42377f0
Eli Goldberg <lgdragon28@gmail.com>
2015-11-03 17:40:49 +0200

    work on shop

---
+++ app/partials/shop/payform.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { DatePicker, DropDownMenu, TextField, Paper } from 'material-ui';
+
+export default class PayForm extends React.Component {
+  render () {
+    let listServices = [
+      { payload: '1', text: 'פתיחת חסימות' },
+      { payload: '2', text: 'הלבנה' },
+      { payload: '3', text: 'פירמוט מחשב' },
+      { payload: '4', text: 'הזמנה לוגיסטית' },
+      { payload: '5', text: 'הכשרה' },
+      { payload: '6', text: 'פקודת מבצע' },
+      { payload: '7', text: 'פתיחת קריאה: תשתיות השו"ב' },
+      { payload: '8', text: 'Weekly' },
+      { payload: '9', text: 'Weekly' },
+      { payload: '10', text: 'Weekly' },
+      { payload: '11', text: 'Weekly' },
+      { payload: '12', text: 'Weekly' },
+      { payload: '13', text: 'Weekly' },
+      { payload: '14', text: 'Weekly' },
+      { payload: '15', text: 'Weekly' },
+    ];
+    return (
+      <div>
+      <Paper zDepth={1} className="pay-form col-md-10" rounded={false}>
+      <TextField
+      hintText="מהות התשלום" />
+      <br />
+      <DatePicker
+      hintText="Landscape Dialog"
+      mode="landscape"/>
+      {
+        listServices.map((item) => {
+          console.log(item.text);
+          return <Paper zDepth={1} className="service-block" rounded={false}>{item.text}</Paper>;
+        })
+      }
+      </Paper>
+      </div>
+
+    );
+  }
+}
--- app/views/shop.js
+++ app/views/shop.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PayForm from '../partials/shop/payform';
 
 export default class Shop extends React.Component {
-    componentWillMount () {
-        this.props.changeTitle('Shop');
-    }
-    render () {
-        return (
-            <h1>Shop</h1>
-        );
-    }
+  componentWillMount () {
+    this.props.changeTitle('Shop');
+  }
+  render () {
+    return (
+      <div>
+        <h1>Shop</h1>
+        <PayForm />
+      </div>
+    );
+  }
 }
