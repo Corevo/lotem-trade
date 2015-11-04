de1d6cb27164818418500efe8820a08e3233b8b2
Michael Kuts <mkuts12@gmail.com>
2015-11-04 11:58:13 +0200

    linting

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 import path from 'path';
 import fs from 'fs';
 
-import transactions from './lib/routes/transaction'
-import home from './lib/routes/home'
+import transactions from './lib/routes/transaction';
+import home from './lib/routes/home';
 
 let port = 3000;
 let app = express();
@@ -14,13 +14,9 @@ app.use('/assets', express.static(STATIC_DIR));
 app.use('/', home);
 app.use('/transactions', transactions);
 
-app.use('*', function(req, res) {
-  res.redirect('/');
-});
-
 // Start the server.
 let server = app.listen(port, () => {
 
-  let { address, port } = server.address();
-  console.log(`Server listening at http://${address}:${port}`);
+  let { address } = server.address();
+  // console.log(`Server listening at http:\/\/${address}:${port}`);
 });
