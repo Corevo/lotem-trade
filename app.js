291c3bb16fa4af75c18066faac77ff1b5fd32730
Michael Kuts <mkuts12@gmail.com>
2015-11-04 20:18:33 +0200

    working api. sortof

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import path from 'path';
 import fs from 'fs';
 import log from './lib/log.js';
+import models from './lib/datasource/models.js';
 
 import transactions from './lib/routes/transaction';
-import home from './lib/routes/home';
+import user from './lib/routes/user.js';
+import account from './lib/routes/account.js';
 
 let port = 8080;
 let app = express();
@@ -12,8 +14,9 @@ let app = express();
 const STATIC_DIR = path.join(__dirname, 'public/assets');
 app.use('/assets', express.static(STATIC_DIR));
 
-app.use('/api', home);
+app.use('/api/user', user);
 app.use('/api/transactions', transactions);
+app.use('/api/account', account);
 
 app.use('*', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
@@ -21,7 +24,6 @@ app.use('*', function(req, res) {
 
 // Start the server.
 let server = app.listen(port, () => {
-
-  let { address } = server.address();
-  log.info(`Server listening at http:\/\/${address}:${port}`);
+    let { address } = server.address();
+    log.info(`Server listening at http:\/\/${address}:${port}`);
 });
