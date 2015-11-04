0b911fccd3b60ae16e8afb132cf48aaa29b080e9
Tomer <gmtomer@gmail.com>
2015-11-04 10:42:15 +0200

    fixed touch interactions

--- app/app.js
+++ app/app.js
@@ -13,6 +13,14 @@ import SideMenu from './partials/global/side-menu';
 import Overview from './views/overview';
 import Shop from './views/shop';
 
+let injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 let store = compose(
     reduxReactRouter({ createHistory })
 )(createStore)(coinApp);
